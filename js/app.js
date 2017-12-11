bd05c8a6e8a34a59f1ce5a79cc812a50f278d347
andrea-isabel <andrea19.93.15@gmail.com>
2017-12-11 09:46:05 -0500

    hide Menu

--- js/app.js
+++ js/app.js
@@ -4,8 +4,16 @@ window.onload = function () {
   var navMenu = document.getElementById('nav-menu');
   
   iconMenu.addEventListener('click', showMenu);
+  document.addEventListener('click', hideMenu);
 
   function showMenu(event) {
     navMenu.classList.toggle('hidden');
   }
+
+  function hideMenu(event) {
+    if (event.target !== iconMenu && (event.target !== navMenu && event.target.closest('nav') !== navMenu) && !(navMenu.classList.contains('hidden'))) {
+      navMenu.classList.add('hidden');
+    }
+  }
+
 };
